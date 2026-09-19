c01a523a8cc91827da48730359021a865c0b1e3a
jvolkening <jdv@base2bio.com>
2026-09-19 15:26:27 -0500

    try to fix new local errors on OSX (can't test locally)

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,6 +11,9 @@ if [[ "${target_platform}" == osx-* ]]; then
   if [[ "${target_platform}" == osx-64 ]]; then
     CFLAGS="${CFLAGS} -D_DARWIN_FEATURE_CLOCK_GETTIME=0"
   fi
+  # avoid locale errors on OSX
+  export LANG=C
+  export LC_ALL=C
   ccflags="${CFLAGS} -fno-common -DPERL_DARWIN -no-cpp-precomp -Werror=partial-availability -D_DARWIN_FEATURE_CLOCK_GETTIME=0 -fno-strict-aliasing -pipe -fstack-protector-strong -DPERL_USE_SAFE_PUTENV ${archflags} ${CPPFLAGS}"
 elif [[ "${target_platform}" == linux-* ]]; then
   ccflags="${CFLAGS} -D_REENTRANT -D_GNU_SOURCE -fwrapv -fno-strict-aliasing -pipe -fstack-protector-strong -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -D_FORTIFY_SOURCE=2"
